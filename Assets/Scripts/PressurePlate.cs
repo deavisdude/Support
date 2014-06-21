5b32da2dfe4f083f5ccec737885516134b41c4e9
Stephen Borden <skateborden@gmail.com>
2014-06-21 00:18:46 -0400

    Added an obstacle script and added it to the prefab. Added it's interactions with the pressure plate.

--- Assets/Scripts/PressurePlate.cs
+++ Assets/Scripts/PressurePlate.cs
@@ -8,10 +8,23 @@ public class PressurePlate : MonoBehaviour {
 	void OnCollisionEnter2D (Collision2D collision)
 	{
 		activated = true;
+		StopAllCoroutines();
 	}
 
 	void OnCollisionExit2D (Collision2D collision)
 	{
 		activated = false;
+	}
+
+	public void Deactivate(float waitTime, float deactivateTimeLength)
+	{
+		StartCoroutine(WaitAndDeactivate(waitTime, deactivateTimeLength));
+	}
+
+	private IEnumerator WaitAndDeactivate(float waitTime, float deactivateTimeLength)
+	{
+		yield return new WaitForSeconds(waitTime);
+
+		// start deactivation
 	}
 }
