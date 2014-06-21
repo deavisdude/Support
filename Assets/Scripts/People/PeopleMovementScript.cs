274fa256228dc8882bbbf627de4425394c4ad53d
Davis Odom <deavisdude@gmail.com>
2014-06-21 14:46:40 -0400

    finally got movement to work

--- Assets/Scripts/People/PeopleMovementScript.cs
+++ Assets/Scripts/People/PeopleMovementScript.cs
@@ -8,7 +8,7 @@ public abstract class PeopleMovementScript : MonoBehaviour
 	// ==================================================
 	
 	public const float ACCELERATION_FACTOR = 1000;
-	public Vector2 MAX_SPEED = new Vector2(.1f, .1f);
+	public Vector2 MAX_SPEED = new Vector2(5, 5);
 	public const float JUMP_POWER = 5f;
 	
 	// ==================================================
