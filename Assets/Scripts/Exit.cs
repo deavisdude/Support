de9c31f83e1d17bd1e705d86e9dc37d44b4efc72
Stephen Borden <skateborden@gmail.com>
2014-06-21 15:38:08 -0400

    Added Aaaron's taunt animation to the enemy

--- Assets/Scripts/Exit.cs
+++ Assets/Scripts/Exit.cs
@@ -5,9 +5,12 @@ public class Exit : MonoBehaviour {
 
 	void OnTriggerEnter2D (Collider2D other)
 	{
-		if(Application.loadedLevel + 1 < Application.levelCount)
-			Application.LoadLevel(Application.loadedLevel + 1);
-		else
-			Application.LoadLevel(0);
+		if(other.gameObject.layer == LayerMask.NameToLayer("player"))
+		{
+			if(Application.loadedLevel + 1 < Application.levelCount)
+				Application.LoadLevel(Application.loadedLevel + 1);
+			else
+				Application.LoadLevel(0);
+		}
 	}
 }
