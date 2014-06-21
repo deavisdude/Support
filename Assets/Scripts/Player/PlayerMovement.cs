aeeabe986c3129a24849b267f1f96219cd09bb0d
Tamby Kojak <development@tambykojak.com>
2014-06-21 00:30:55 -0400

    adding to player movement script

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -4,21 +4,50 @@ using System.Collections;
 public class PlayerMovement : MonoBehaviour
 {
 	// ==================================================
+	// Constants
+	// ==================================================
+
+	private Vector3 mHorizontalAccelerationChange = new Vector3 (30, 0, 0);
+
+	// ==================================================
 	// Variables
 	// ==================================================
+
+	public Vector3 mAcceleration = new Vector3 (0, 0, 0);
+	private Vector3 mVelocity = new Vector3 (0, 0, 0);
 
 	// ==================================================
 	// Methods
 	// ==================================================
 
-	void Start ()
+
+	private void jump ()
 	{
-	
+
 	}
 
-	// Update is called once per frame
-	void Update ()
+	private void adjustAcceleration ()
 	{
+		if (Input.GetKey (KeyCode.RightArrow)) {
+			mAcceleration += mHorizontalAccelerationChange * Time.deltaTime;
+		} else if (Input.GetKey (KeyCode.LeftArrow)) {
+			mAcceleration -= mHorizontalAccelerationChange * Time.deltaTime;
+		} else {
+			mAcceleration *= 0;
+			mVelocity /= 2f * Time.deltaTime;
+		}
+
+		Vector3 displacement = mVelocity * Time.deltaTime;
+		transform.position += displacement;
+		mVelocity += mAcceleration * Time.deltaTime;
+	}
+
+	// =========================
+	// Lifecycle Methods
+	// =========================
 	
+	protected void Update ()
+	{
+		adjustAcceleration ();
 	}
 }
