84cead4bc9787154793500f5dde89473f6d48b4d
Tamby Kojak <development@tambykojak.com>
2014-06-21 11:15:49 -0400

    added follow script, made abstract "people script" since follower and player are similar

---
+++ Assets/Scripts/People/Follower/FollowerMovementScript.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowerMovementScript : PeopleMovementScript
+{
+	// ==================================================
+	// Constants
+	// ==================================================
+	
+	// ==================================================
+	// Variables
+	// ==================================================
+
+	private bool mFollowPlayer = false;
+	private GameObject mPlayer;
+
+	// ==================================================
+	// Methods
+	// ==================================================
+
+	override public void handleJump ()
+	{
+
+	}
+
+	override public void handleMovement ()
+	{
+
+	}
+
+	// =========================
+	// Lifecycle Methods
+	// =========================
+
+	protected void Start ()
+	{
+		mPlayer = GameObject.FindGameObjectWithTag ("Player");
+	}
+}
