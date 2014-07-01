fa0d5243f0a654ef16b5dcfbe92e8e352d41327f
Tamby Kojak <development@tambykojak.com>
2014-07-01 08:00:00 -0400

    fixed up all the menu stuff

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -1,112 +1,129 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PauseMenu : SPSUGameJamScript {
+public class PauseMenu : SPSUGameJamScript
+{
 
-	private enum Buttons {menu, unpause, quit};
-	private Buttons currentButton;
-
-	public static bool paused = false;
-	public GameObject background;
-	public GameObject menuButtonSelector;
-	public GameObject unpauseButtonSelector;
-	public GameObject quitButtonSelector;
-
-	private float selectionTime;
-	private float buttonTimeOut = .25f;
-
-	// Use this for initialization
-	new void Start () {
-		background.SetActive(false);
-		base.Start();
-		currentButton = Buttons.unpause;
-	}
-
-	void OnEnable ()
-	{
-		menuButtonSelector.SetActive(false);
-		unpauseButtonSelector.SetActive(true);
-		quitButtonSelector.SetActive(false);
-	}
-	
-	// Update is called once per frame
-	void Update ()
-	{
-		if (Input.GetKeyDown (KeyCode.Escape))
+		private enum Buttons
 		{
-			if(CreditsScript.creditsShowing)
-				return;
+				menu,
+				unpause,
+				quit}
+		;
+		private Buttons currentButton;
 
-			Pause(!paused);
+		public static bool paused = false;
+		public GameObject background;
+
+		public GameObject menuButton;
+		public GameObject unpauseButton;
+		public GameObject quitButton;
+
+		public GameObject menuButtonSelector;
+		public GameObject unpauseButtonSelector;
+		public GameObject quitButtonSelector;
+
+		private float selectionTime;
+		private float buttonTimeOut = .25f;
+
+		// Use this for initialization
+		new void Start ()
+		{
+				background.SetActive (false);
+				base.Start ();
+				currentButton = Buttons.unpause;
 		}
 
-		if(paused)
+		void OnEnable ()
 		{
-			float timeoutTimer = Time.realtimeSinceStartup - selectionTime;
+				menuButtonSelector.SetActive (false);
+				unpauseButtonSelector.SetActive (true);
+				quitButtonSelector.SetActive (false);
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+				if (Input.GetKeyDown (KeyCode.Escape)) {
+						if (CreditsScript.creditsShowing)
+								return;
 
-			if (Input.GetAxisRaw ("Horizontal") > 0 && timeoutTimer >= buttonTimeOut) 
-			{
-				if((int)currentButton < System.Enum.GetValues(typeof(Buttons)).Length - 1)
-				{
-					currentButton++;
-					SelectButton(currentButton);
+						Pause (!paused);
 				}
-			} 
-			else if (Input.GetAxisRaw ("Horizontal") < 0 && timeoutTimer >= buttonTimeOut) 
-			{
-				if((int)currentButton > 0)
-				{
-					currentButton--;
-					SelectButton(currentButton);
-				}
-			}
+
+				if (paused) {
+						float timeoutTimer = Time.realtimeSinceStartup - selectionTime;
+
+						if (Input.GetAxisRaw ("Horizontal") > 0 && timeoutTimer >= buttonTimeOut) {
+								if ((int)currentButton < System.Enum.GetValues (typeof(Buttons)).Length - 1) {
+										currentButton++;
+										SelectButton (currentButton);
+								}
+						} else if (Input.GetAxisRaw ("Horizontal") < 0 && timeoutTimer >= buttonTimeOut) {
+								if ((int)currentButton > 0) {
+										currentButton--;
+										SelectButton (currentButton);
+								}
+						}
 			
-			if (Input.GetButtonUp ("Jump") || Input.GetKey (KeyCode.Return)) {
-				if(menuButtonSelector.activeInHierarchy)
-					LoadMenu();
-				else if(unpauseButtonSelector.activeInHierarchy)
-					Pause (false);
-				else if(quitButtonSelector.activeInHierarchy)
-					Application.Quit();
-			}
+						if (Input.GetButtonUp ("Jump") || Input.GetKey (KeyCode.Return)) {
+								if (menuButtonSelector.activeInHierarchy)
+										LoadMenu ();
+								else if (unpauseButtonSelector.activeInHierarchy)
+										Pause (false);
+								else if (quitButtonSelector.activeInHierarchy)
+										Application.Quit ();
+						}
+				}
 		}
-	}
 
-	private void SelectButton(Buttons button)
-	{
-		selectionTime = Time.realtimeSinceStartup;
-		audioManager.playMenuSound();
-		menuButtonSelector.SetActive(button == Buttons.menu);
-		unpauseButtonSelector.SetActive(button == Buttons.unpause);
-		quitButtonSelector.SetActive(button == Buttons.quit);
-	}
-	
-	private void Pause(bool pause)
-	{
-		if(pause)
+		private void SelectButton (Buttons button)
 		{
-			paused = true;
-			Time.timeScale = 0;
-			audioManager.Pause(true);
-			background.SetActive(true);
+				selectionTime = Time.realtimeSinceStartup;
+				audioManager.playMenuSound ();
+
+				menuButtonSelector.SetActive (button == Buttons.menu);
+				menuButton.renderer.material.color = new Color (menuButton.renderer.material.color.r,
+		                                                menuButton.renderer.material.color.r,
+		                                                menuButton.renderer.material.color.r,
+		                                                (button != Buttons.menu) ? 1f : 0f);
+
+				unpauseButtonSelector.SetActive (button == Buttons.unpause);
+				unpauseButton.renderer.material.color = new Color (unpauseButton.renderer.material.color.r,
+		                                                   unpauseButton.renderer.material.color.r,
+		                                                   unpauseButton.renderer.material.color.r,
+		                                                (button != Buttons.unpause) ? 1f : 0f);
+
+				quitButtonSelector.SetActive (button == Buttons.quit);
+				quitButton.renderer.material.color = new Color (quitButton.renderer.material.color.r,
+		                                                quitButton.renderer.material.color.r,
+		                                                quitButton.renderer.material.color.r,
+		                                                (button != Buttons.quit) ? 1f : 0f);
 		}
-		else
-		{
-			paused = false;
-			Time.timeScale = 1;
-			audioManager.Pause(false);
-			background.SetActive(false);
-		}
-	}
 	
-	private void LoadMenu()
-	{
-		Time.timeScale = 1;
-		paused = false;
-		Application.LoadLevel ("menu");
-		audioManager.StopRainLoop();
-		audioManager.StopGameMusic();
-		audioManager.StopBirds();
-		audioManager.FadeMenuMusicIn();
-	}
+		private void Pause (bool pause)
+		{
+				if (pause) {
+						paused = true;
+						Time.timeScale = 0;
+						audioManager.Pause (true);
+						background.SetActive (true);
+				} else {
+						paused = false;
+						Time.timeScale = 1;
+						audioManager.Pause (false);
+						background.SetActive (false);
+				}
+		}
+	
+		private void LoadMenu ()
+		{
+				Time.timeScale = 1;
+				paused = false;
+				Application.LoadLevel ("menu");
+				audioManager.StopRainLoop ();
+				audioManager.StopGameMusic ();
+				audioManager.StopBirds ();
+				audioManager.FadeMenuMusicIn ();
+		}
 }
