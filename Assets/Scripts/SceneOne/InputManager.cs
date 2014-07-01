d57c8abeb5d35ceec1d1572de4d99d2835d1efe1
Tamby Kojak <development@tambykojak.com>
2014-07-01 06:33:42 -0400

    changed pause menu selector

--- Assets/Scripts/SceneOne/InputManager.cs
+++ Assets/Scripts/SceneOne/InputManager.cs
@@ -3,83 +3,75 @@ using System.Collections;
 
 public class InputManager : SPSUGameJamScript
 {
-	public SpriteRenderer leftSelector;
-	public SpriteRenderer rightSelector;
-	public GameObject leftRightArrows;
+		public SpriteRenderer leftSelector;
+		public SpriteRenderer rightSelector;
 
-	new void Start ()
-	{
-		base.Start ();
+		new void Start ()
+		{
+				base.Start ();
 
-		Color leftColor = leftSelector.color;
-		leftColor.a = 0.0f;
-		leftSelector.color = leftColor;
+				Color leftColor = leftSelector.color;
+				leftColor.a = 0.0f;
+				leftSelector.color = leftColor;
 
-		Color rightColor = rightSelector.color;
-		rightColor.a = 0.0f;
-		rightSelector.color = rightColor;
-	}
-
-	void Update ()
-	{
-		if (Input.GetAxis ("Horizontal") > 0) {
-			showRight ();
-		} else if (Input.GetAxis ("Horizontal") < 0) {
-			showLeft ();
-		}
-		if (Input.GetButton ("Jump") || Input.GetKey (KeyCode.Return)) {
-			if (leftSelector.color.a == 1f) {
-				PlayerMovement.isBoy = true;
-			} else if (rightSelector.color.a == 1f) {
-				PlayerMovement.isBoy = false;
-			} else {
-				return;
-			}
-
-			Obstacle.enemyIsBoy = Random.Range (0, 2) == 0;
-			audioManager.playLevelUpSound();
-			audioManager.startGameMusic ();
-
-			Application.LoadLevel ("lvl1");
+				Color rightColor = rightSelector.color;
+				rightColor.a = 0.0f;
+				rightSelector.color = rightColor;
 		}
 
-		if (Input.GetKeyDown (KeyCode.Escape)) {
-			Application.Quit ();
+		void Update ()
+		{
+				if (Input.GetAxis ("Horizontal") > 0) {
+						showRight ();
+				} else if (Input.GetAxis ("Horizontal") < 0) {
+						showLeft ();
+				}
+				if (Input.GetButton ("Jump") || Input.GetKey (KeyCode.Return)) {
+						if (leftSelector.color.a == 1f) {
+								PlayerMovement.isBoy = true;
+						} else if (rightSelector.color.a == 1f) {
+								PlayerMovement.isBoy = false;
+						} else {
+								return;
+						}
+
+						Obstacle.enemyIsBoy = Random.Range (0, 2) == 0;
+						audioManager.playLevelUpSound ();
+						audioManager.startGameMusic ();
+
+						Application.LoadLevel ("lvl1");
+				}
+
+				if (Input.GetKeyDown (KeyCode.Escape)) {
+						Application.Quit ();
+				}
 		}
-	}
 	
-	private void showRight ()
-	{
-		Color rightColor = rightSelector.color;
+		private void showRight ()
+		{
+				Color rightColor = rightSelector.color;
 
-		if(rightColor.a == 0)
-			audioManager.playMenuSound();
+				if (rightColor.a == 0)
+						audioManager.playMenuSound ();
 
-		rightSelector.color = Color.black;
+				rightSelector.color = Color.black;
 
-		Color leftColor = leftSelector.color;
-		leftColor.a = 0.0f;
-		leftSelector.color = leftColor;
-		hideArrows ();
-	}
+				Color leftColor = leftSelector.color;
+				leftColor.a = 0.0f;
+				leftSelector.color = leftColor;
+		}
 	
-	private void showLeft ()
-	{
-		Color rightColor = rightSelector.color;
-		rightColor.a = 0.0f;
-		rightSelector.color = rightColor;
+		private void showLeft ()
+		{
+				Color rightColor = rightSelector.color;
+				rightColor.a = 0.0f;
+				rightSelector.color = rightColor;
 		
-		Color leftColor = leftSelector.color;
+				Color leftColor = leftSelector.color;
 
-		if(leftColor.a == 0)
-			audioManager.playMenuSound();
+				if (leftColor.a == 0)
+						audioManager.playMenuSound ();
 
-		leftSelector.color = Color.black;
-		hideArrows ();
-	}
-
-	private void hideArrows ()
-	{
-		leftRightArrows.SetActive(false);
-	}
+				leftSelector.color = Color.black;
+		}
 }
