eae9655b2554e3489e348940f29473b8787135ea
Tamby Kojak <development@tambykojak.com>
2014-07-01 06:26:29 -0400

    hmmmmm... seems like collision count is never becoming 0, therefore never getting triggered to deactivate.

--- Assets/Scripts/PressurePlate.cs
+++ Assets/Scripts/PressurePlate.cs
@@ -4,98 +4,93 @@ using Holoville.HOTween;
 
 public class PressurePlate : SPSUGameJamScript
 {
-	public bool activated = false;
-	public SpriteRenderer spriteRender;
-	private Vector3 defaultScale;
-	private Vector3 activatedScale;
-	public int collisionCount = 0;
-	public Transform sprite;
+		public bool activated = false;
+		public SpriteRenderer spriteRender;
+		private Vector3 defaultScale;
+		private Vector3 activatedScale;
+		public int collisionCount = 0;
+		public Transform sprite;
+		public bool isTriggered = false;
 
-	void Awake ()
-	{
-		if (spriteRender == null)
-			spriteRender = GetComponent<SpriteRenderer> ();
-
-		defaultScale = sprite.localScale;
-		activatedScale = new Vector3(defaultScale.x, defaultScale.y * 0.75f, defaultScale.z);
-	}
-
-
-	void OnTriggerEnter2D (Collider2D collider)
-	{
-		if ((collider.gameObject.layer == LayerMask.NameToLayer ("player") 
-		     || collider.gameObject.layer == LayerMask.NameToLayer ("followingPeople")) 
-		     && collider.GetType() == typeof(CircleCollider2D))
+		void Awake ()
 		{
-			collisionCount++;
+				if (spriteRender == null)
+						spriteRender = GetComponent<SpriteRenderer> ();
 
-			if (!PressurePlateManager.DontChangePlateState() && !activated && collisionCount == 1) 
-			{
-				activated = true;
-				sprite.localScale = activatedScale;
+				defaultScale = sprite.localScale;
+				activatedScale = new Vector3 (defaultScale.x, defaultScale.y * 0.75f, defaultScale.z);
+		}
 
-				if(collider.gameObject.layer == LayerMask.NameToLayer ("followingPeople"))
-					audioManager.playPressurePlateActivatedSound (0.5f);
-				else if(collider.gameObject.layer == LayerMask.NameToLayer ("player"))
-				{
-					audioManager.playPressurePlateActivatedSound ();
+
+		void OnTriggerEnter2D (Collider2D collider)
+		{
+				if ((collider.gameObject.layer == LayerMask.NameToLayer ("player") 
+						|| collider.gameObject.layer == LayerMask.NameToLayer ("followingPeople")) 
+						&& collider.GetType () == typeof(CircleCollider2D)) {
+
+						if (!PressurePlateManager.DontChangePlateState () && !activated && !isTriggered) {
+								activated = true;
+								sprite.localScale = activatedScale;
+
+								if (collider.gameObject.layer == LayerMask.NameToLayer ("followingPeople"))
+										audioManager.playPressurePlateActivatedSound (0.5f);
+								else if (collider.gameObject.layer == LayerMask.NameToLayer ("player")) {
+										audioManager.playPressurePlateActivatedSound ();
+								}
+
+								spriteRender.color = Color.green;
+								StopAllCoroutines ();
+								isTriggered = true;
+						}
 				}
-
-				spriteRender.color = Color.green;
-				StopAllCoroutines ();
-			}
 		}
-	}
 
-	void OnTriggerExit2D (Collider2D collider)
-	{
-		if ((collider.gameObject.layer == LayerMask.NameToLayer ("player") 
-		     || collider.gameObject.layer == LayerMask.NameToLayer ("followingPeople"))
-		     && collider.GetType() == typeof(CircleCollider2D))
+		void OnTriggerExit2D (Collider2D collider)
 		{
-			if(collisionCount > 0)
-				collisionCount--;
+				if ((collider.gameObject.layer == LayerMask.NameToLayer ("player") 
+						|| collider.gameObject.layer == LayerMask.NameToLayer ("followingPeople"))
+						&& collider.GetType () == typeof(CircleCollider2D)) {
 
-			if (!PressurePlateManager.DontChangePlateState() && collisionCount == 0) {
+						if (!PressurePlateManager.DontChangePlateState () && isTriggered) {
+								isTriggered = false;
+								DeactivateNow (false);
 
-				DeactivateNow (false);
-
-				if(collider.gameObject.layer == LayerMask.NameToLayer ("followingPeople"))
-					audioManager.playPressurePlateDeactivedSound (0.5f);
-				else if(collider.gameObject.layer == LayerMask.NameToLayer ("player"))
-					audioManager.playPressurePlateDeactivedSound ();
-			}
+								if (collider.gameObject.layer == LayerMask.NameToLayer ("followingPeople"))
+										audioManager.playPressurePlateDeactivedSound (0.5f);
+								else if (collider.gameObject.layer == LayerMask.NameToLayer ("player"))
+										audioManager.playPressurePlateDeactivedSound ();
+						}
+				}
 		}
-	}
 
-	public void DeactivateTimed (float waitTime, float deactivateTimeLength)
-	{
-		StartCoroutine (WaitAndDeactivate (waitTime, deactivateTimeLength));
-	}
+		public void DeactivateTimed (float waitTime, float deactivateTimeLength)
+		{
+				StartCoroutine (WaitAndDeactivate (waitTime, deactivateTimeLength));
+		}
 
-	private IEnumerator WaitAndDeactivate (float waitTime, float deactivateTimeLength)
-	{
-		yield return new WaitForSeconds (waitTime);
+		private IEnumerator WaitAndDeactivate (float waitTime, float deactivateTimeLength)
+		{
+				yield return new WaitForSeconds (waitTime);
 
-		TweenParms tween = new TweenParms ().Prop ("color", Color.white).OnComplete (DeactivateNow);
-		HOTween.To (spriteRender, deactivateTimeLength, tween);
-		HOTween.To(sprite, deactivateTimeLength, "localScale", defaultScale);
-	}
+				TweenParms tween = new TweenParms ().Prop ("color", Color.white).OnComplete (DeactivateNow);
+				HOTween.To (spriteRender, deactivateTimeLength, tween);
+				HOTween.To (sprite, deactivateTimeLength, "localScale", defaultScale);
+		}
 
-	// with sound called by the timer tween OnComplete
-	private void DeactivateNow()
-	{
-		DeactivateNow(true);
-	}
+		// with sound called by the timer tween OnComplete
+		private void DeactivateNow ()
+		{
+				DeactivateNow (true);
+		}
 
-	// without sound called by OnTriggerExit, because it plays a sound at a volume depending on whether it's the player or a NPC
-	private void DeactivateNow (bool playSound)
-	{
-		activated = false;
-		spriteRender.color = Color.white;
-		sprite.localScale = defaultScale;
+		// without sound called by OnTriggerExit, because it plays a sound at a volume depending on whether it's the player or a NPC
+		private void DeactivateNow (bool playSound)
+		{
+				activated = false;
+				spriteRender.color = Color.white;
+				sprite.localScale = defaultScale;
 
-		if(playSound)
-			audioManager.playPressurePlateDeactivedSound(2);
-	}
+				if (playSound)
+						audioManager.playPressurePlateDeactivedSound (2);
+		}
 }
--- Assets/Scripts/PressurePlateManager.cs
+++ Assets/Scripts/PressurePlateManager.cs
@@ -4,78 +4,73 @@ using System.Collections;
 public class PressurePlateManager : MonoBehaviour
 {
 
-	public PressurePlate[] pressurePlates;
+		public PressurePlate[] pressurePlates;
 
-	public CreditsScript creditsScript;
+		public CreditsScript creditsScript;
 
-	private bool _allPlatesActive = false;
-	public Obstacle obstacle;
+		private bool _allPlatesActive = false;
+		public Obstacle obstacle;
 
-	private static bool waitingToStartTimer = false;
-	private static bool _timerStarted = false;
-	public static bool timerStarted {
-		get { return _timerStarted; }
-	}
+		private static bool waitingToStartTimer = false;
+		private static bool _timerStarted = false;
+		public static bool timerStarted {
+				get { return _timerStarted; }
+		}
 
-	void OnLevelWasLoaded (int level)
-	{
-		_allPlatesActive = false;
-		waitingToStartTimer = false;
-		_timerStarted = false;
-	}
-
-	public static bool DontChangePlateState()
-	{
-		return waitingToStartTimer || timerStarted;
-	}
-
-	// Update is called once per frame
-	void Update ()
-	{
-		_allPlatesActive = true;
-		foreach (PressurePlate plate in pressurePlates) 
+		void OnLevelWasLoaded (int level)
 		{
-			if (!plate.activated || plate.collisionCount == 0) {
 				_allPlatesActive = false;
-				break;
-			}
+				waitingToStartTimer = false;
+				_timerStarted = false;
 		}
 
-		if (_allPlatesActive && !waitingToStartTimer && !timerStarted) 
+		public static bool DontChangePlateState ()
 		{
-			if (Application.loadedLevel != 4) {
-				// tell obstacle to shrink
-				obstacle.Shrink ();
+				return waitingToStartTimer || timerStarted;
+		}
 
-				waitingToStartTimer = true;
-			}
-			else  // level 4
-			{
-				if (creditsScript != null) {
-					creditsScript.startFadingIn ();
+		// Update is called once per frame
+		void Update ()
+		{
+				_allPlatesActive = true;
+				foreach (PressurePlate plate in pressurePlates) {
+						if (!plate.activated || !plate.isTriggered) {
+								_allPlatesActive = false;
+								break;
+						}
 				}
-			}
+
+				if (_allPlatesActive && !waitingToStartTimer && !timerStarted) {
+						if (Application.loadedLevel != 4) {
+								// tell obstacle to shrink
+								obstacle.Shrink ();
+
+								waitingToStartTimer = true;
+						} else {  // level 4
+								if (creditsScript != null) {
+										creditsScript.startFadingIn ();
+								}
+						}
+				}
+
+				if (!_allPlatesActive && waitingToStartTimer && !timerStarted && obstacle.timed) {
+						_timerStarted = true;
+						waitingToStartTimer = false;
+
+						obstacle.StartGrowBackTimer ();
+
+						for (int i = (pressurePlates.Length - 1); i >= 0; i--) {
+								float plateDeactivationTime = obstacle.growBackWaitTime / pressurePlates.Length;
+								pressurePlates [i].DeactivateTimed (plateDeactivationTime * (pressurePlates.Length - (i + 1)), plateDeactivationTime);
+						}
+						StartCoroutine (WaitAndDeactivate (obstacle.growBackWaitTime));
+				}
 		}
 
-		if(!_allPlatesActive && waitingToStartTimer && !timerStarted && obstacle.timed)
+		private IEnumerator WaitAndDeactivate (float wait)
 		{
-			_timerStarted = true;
-			waitingToStartTimer = false;
-
-			obstacle.StartGrowBackTimer();
-
-			for (int i = (pressurePlates.Length - 1); i >= 0; i--) {
-				float plateDeactivationTime = obstacle.growBackWaitTime / pressurePlates.Length;
-				pressurePlates [i].DeactivateTimed (plateDeactivationTime * (pressurePlates.Length - (i + 1)), plateDeactivationTime);
-			}
-			StartCoroutine (WaitAndDeactivate (obstacle.growBackWaitTime));
+				yield return new WaitForSeconds (wait);
+				waitingToStartTimer = false;
+				_timerStarted = false;
 		}
-	}
-
-	private IEnumerator WaitAndDeactivate (float wait)
-	{
-		yield return new WaitForSeconds (wait);
-		waitingToStartTimer = false;
-		_timerStarted = false;
-	}
 }
