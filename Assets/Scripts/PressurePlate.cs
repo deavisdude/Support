aaf64ecf819cad192e54f435d42d28cfc227bc89
Stephen Borden <skateborden@gmail.com>
2014-07-01 18:18:59 -0400

    I was seeing the thing where you can get a second jump on the sides of the platforms on lvl3 so I made them a bit narrower and that seems to fix it. I reverted the pressure plate scripts. I don't know what's going on with them on your machine Tamby, but these work for me. I'm going to build this now.

--- Assets/Scripts/PressurePlate.cs
+++ Assets/Scripts/PressurePlate.cs
@@ -4,93 +4,98 @@ using Holoville.HOTween;
 
 public class PressurePlate : SPSUGameJamScript
 {
-		public bool activated = false;
-		public SpriteRenderer spriteRender;
-		private Vector3 defaultScale;
-		private Vector3 activatedScale;
-		public int collisionCount = 0;
-		public Transform sprite;
-		public bool isTriggered = false;
+	public bool activated = false;
+	public SpriteRenderer spriteRender;
+	private Vector3 defaultScale;
+	private Vector3 activatedScale;
+	public int collisionCount = 0;
+	public Transform sprite;
 
-		void Awake ()
+	void Awake ()
+	{
+		if (spriteRender == null)
+			spriteRender = GetComponent<SpriteRenderer> ();
+
+		defaultScale = sprite.localScale;
+		activatedScale = new Vector3(defaultScale.x, defaultScale.y * 0.75f, defaultScale.z);
+	}
+
+
+	void OnTriggerEnter2D (Collider2D collider)
+	{
+		if ((collider.gameObject.layer == LayerMask.NameToLayer ("player") 
+		     || collider.gameObject.layer == LayerMask.NameToLayer ("followingPeople")) 
+		     && collider.GetType() == typeof(CircleCollider2D))
 		{
-				if (spriteRender == null)
-						spriteRender = GetComponent<SpriteRenderer> ();
+			collisionCount++;
 
-				defaultScale = sprite.localScale;
-				activatedScale = new Vector3 (defaultScale.x, defaultScale.y * 0.75f, defaultScale.z);
-		}
+			if (!PressurePlateManager.DontChangePlateState() && !activated && collisionCount == 1) 
+			{
+				activated = true;
+				sprite.localScale = activatedScale;
 
-
-		void OnTriggerEnter2D (Collider2D collider)
-		{
-				if ((collider.gameObject.layer == LayerMask.NameToLayer ("player") 
-						|| collider.gameObject.layer == LayerMask.NameToLayer ("followingPeople")) 
-						&& collider.GetType () == typeof(CircleCollider2D)) {
-
-						if (!PressurePlateManager.DontChangePlateState () && !activated && !isTriggered) {
-								activated = true;
-								sprite.localScale = activatedScale;
-
-								if (collider.gameObject.layer == LayerMask.NameToLayer ("followingPeople"))
-										audioManager.playPressurePlateActivatedSound (0.5f);
-								else if (collider.gameObject.layer == LayerMask.NameToLayer ("player")) {
-										audioManager.playPressurePlateActivatedSound ();
-								}
-
-								spriteRender.color = Color.green;
-								StopAllCoroutines ();
-								isTriggered = true;
-						}
+				if(collider.gameObject.layer == LayerMask.NameToLayer ("followingPeople"))
+					audioManager.playPressurePlateActivatedSound (0.5f);
+				else if(collider.gameObject.layer == LayerMask.NameToLayer ("player"))
+				{
+					audioManager.playPressurePlateActivatedSound ();
 				}
-		}
 
-		void OnTriggerExit2D (Collider2D collider)
+				spriteRender.color = Color.green;
+				StopAllCoroutines ();
+			}
+		}
+	}
+
+	void OnTriggerExit2D (Collider2D collider)
+	{
+		if ((collider.gameObject.layer == LayerMask.NameToLayer ("player") 
+		     || collider.gameObject.layer == LayerMask.NameToLayer ("followingPeople"))
+		     && collider.GetType() == typeof(CircleCollider2D))
 		{
-				if ((collider.gameObject.layer == LayerMask.NameToLayer ("player") 
-						|| collider.gameObject.layer == LayerMask.NameToLayer ("followingPeople"))
-						&& collider.GetType () == typeof(CircleCollider2D)) {
+			if(collisionCount > 0)
+				collisionCount--;
 
-						if (!PressurePlateManager.DontChangePlateState () && isTriggered) {
-								isTriggered = false;
-								DeactivateNow (false);
+			if (!PressurePlateManager.DontChangePlateState() && collisionCount == 0) {
 
-								if (collider.gameObject.layer == LayerMask.NameToLayer ("followingPeople"))
-										audioManager.playPressurePlateDeactivedSound (0.5f);
-								else if (collider.gameObject.layer == LayerMask.NameToLayer ("player"))
-										audioManager.playPressurePlateDeactivedSound ();
-						}
-				}
+				DeactivateNow (false);
+
+				if(collider.gameObject.layer == LayerMask.NameToLayer ("followingPeople"))
+					audioManager.playPressurePlateDeactivedSound (0.5f);
+				else if(collider.gameObject.layer == LayerMask.NameToLayer ("player"))
+					audioManager.playPressurePlateDeactivedSound ();
+			}
 		}
+	}
 
-		public void DeactivateTimed (float waitTime, float deactivateTimeLength)
-		{
-				StartCoroutine (WaitAndDeactivate (waitTime, deactivateTimeLength));
-		}
+	public void DeactivateTimed (float waitTime, float deactivateTimeLength)
+	{
+		StartCoroutine (WaitAndDeactivate (waitTime, deactivateTimeLength));
+	}
 
-		private IEnumerator WaitAndDeactivate (float waitTime, float deactivateTimeLength)
-		{
-				yield return new WaitForSeconds (waitTime);
+	private IEnumerator WaitAndDeactivate (float waitTime, float deactivateTimeLength)
+	{
+		yield return new WaitForSeconds (waitTime);
 
-				TweenParms tween = new TweenParms ().Prop ("color", Color.white).OnComplete (DeactivateNow);
-				HOTween.To (spriteRender, deactivateTimeLength, tween);
-				HOTween.To (sprite, deactivateTimeLength, "localScale", defaultScale);
-		}
+		TweenParms tween = new TweenParms ().Prop ("color", Color.white).OnComplete (DeactivateNow);
+		HOTween.To (spriteRender, deactivateTimeLength, tween);
+		HOTween.To(sprite, deactivateTimeLength, "localScale", defaultScale);
+	}
 
-		// with sound called by the timer tween OnComplete
-		private void DeactivateNow ()
-		{
-				DeactivateNow (true);
-		}
+	// with sound called by the timer tween OnComplete
+	private void DeactivateNow()
+	{
+		DeactivateNow(true);
+	}
 
-		// without sound called by OnTriggerExit, because it plays a sound at a volume depending on whether it's the player or a NPC
-		private void DeactivateNow (bool playSound)
-		{
-				activated = false;
-				spriteRender.color = Color.white;
-				sprite.localScale = defaultScale;
+	// without sound called by OnTriggerExit, because it plays a sound at a volume depending on whether it's the player or a NPC
+	private void DeactivateNow (bool playSound)
+	{
+		activated = false;
+		spriteRender.color = Color.white;
+		sprite.localScale = defaultScale;
 
-				if (playSound)
-						audioManager.playPressurePlateDeactivedSound (2);
-		}
+		if(playSound)
+			audioManager.playPressurePlateDeactivedSound(2);
+	}
 }
